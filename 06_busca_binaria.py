8961f6a4c3657ae7cbbc78e5d8e2b6de31a7609d
Fausto Cintra <professor@faustocintra.com.br>
2025-08-27 19:07:13 -0300

    (27/08) Busca binária

--- 06_busca_binaria.py
+++ 06_busca_binaria.py
@@ -1,3 +1,6 @@
+# Variável global para a contagem de comparações
+comps = None
+
 def busca_binaria(lista, val):
     """
     ALGORITMO DE BUSCA BINÁRIA
@@ -9,6 +12,10 @@ def busca_binaria(lista, val):
     reste apenas uma sublista vazia, quando então se
     conclui que o valor de busca não existe na lista.
     """
+    # Avisando à função que queremos usar a variável global
+    global comps
+    comps = 0   # Zerando o valor para começar a contar
+
     ini = 0                 # Posição inicial da lista
     fim = len(lista) - 1    # Posição final da lista
 
@@ -21,19 +28,25 @@ def busca_binaria(lista, val):
         # igual ao valor de busca. Em caso afirmativo, 
         # retornamos a posição do meio, pois o valor de busca
         # foi encontrado
-        if val == lista[meio]: return meio
+        if val == lista[meio]: 
+            comps += 1
+            return meio
 
         # Senão, se o valor de busca é MENOR do que o valor do
         # meio, move o ponteiro do fim para a posição anterior
         # à do meio e reinicia a busca na metade ESQUERDA da
         # (sub)lista
-        elif val < lista[meio]: fim = meio - 1
+        elif val < lista[meio]: 
+            comps += 2
+            fim = meio - 1
 
         # Por fim, se o valor de busca é MAIOR do que o valor
         # do meio, move o ponteiro do início para a posição
         # seguinte à do meio e reinicia a busca na metade
         # DIREITA da lista
-        else: ini = meio + 1
+        else: 
+            comps += 2
+            ini = meio + 1
 
     # <-- CUIDADO COM A INDENTAÇÃO AQUI
     # Se chegamos a este ponto, é porque o valor de busca não
@@ -49,9 +62,9 @@ vals = [30, 4, -3]
 for v in vals:
     pos = busca_binaria(nums, v)
     if pos >= 0:
-        print(f"Elemento {v} encontrado na posição {pos}.")
+        print(f"Elemento {v} encontrado na posição {pos}, comparações: {comps}.")
     else:
-        print(f"Elemento {v} não foi encontrado, pois a busca retornou o valor {pos}.")
+        print(f"Elemento {v} não foi encontrado, pois a busca retornou o valor {pos}, comparações: {comps}.")
 
 print('-' * 80)
 
@@ -78,8 +91,8 @@ for n in buscas:
     pos = busca_binaria(nomes, n)
     hora_fim = time()
     if pos >= 0:
-        print(f"Nome {n} encontrado na posição {pos}.")
+        print(f"Nome {n} encontrado na posição {pos}, comparações: {comps}.")
     else:
-        print(f"Nome {n} não encontrado.")
+        print(f"Nome {n} não encontrado, comparações {comps}.")
 
     print(f"Tempo gasto: {(hora_fim - hora_ini) * 1000}ms.\n")
